df928a5d6d2d8e1ab8e15719aef080271c53103d
Marco Valerio <mo.valerio93@gmal.com>
2022-09-01 10:10:29 +0200

    vue-boolflix start

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="bg">
+        <div class="d-flex">
+            <h1>BOOLFLIX</h1>
+            <input type="text"/>
+        </div>
+    </div>  
+</template>
+
+<script>
+export default {
+    name:"MyHeader",
+
+}
+</script>
+
+<style scoped lang="scss">
+   
+    .bg{
+        width: 100%;
+        background-color: black;
+    }   
+
+        div .d-flex{
+            height: 70px;
+            margin: 10px;
+            display: flex;
+            justify-content: space-between;
+
+            h1{
+                color: red;
+            }
+            input{
+                width: 20%;
+                height: 40%;
+                margin-top: 25px;
+            }
+        }
+    
+
+
+</style>
